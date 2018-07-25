34e8508f5b915c0dd81c7e603c9871216c0b8376
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-07-25 11:45:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let arr = ['kirmanj', 'fars', 'web', 'recoded']
 returnFirstTwoDrivers(){
   
   const v1 = Function (arr){
-    return arr.slice(0,2);
+    arr1= arr.slice(0,2);
+    return arr1;
   }
 }
