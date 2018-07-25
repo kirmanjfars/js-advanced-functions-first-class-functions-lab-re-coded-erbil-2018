2594e67a4ffc2e7079335f32303f4dd9ef0f3f61
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-07-25 11:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Code your solution in this file!
 
-let arr = ['kirmanj', 'fars', 'web', 'recoded']
+let drivers = ['kirmanj', 'fars', 'web', 'recoded']
 
 
-const returnFirstTwoDrivers = Function (arr){
+const returnFirstTwoDrivers = Function (drivers){
     arr1= arr.slice(0,2);
     return arr1;
   }
