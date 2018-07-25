75cc345f01fbe8dc2b71aa8d98f6703b947208d7
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-07-25 11:47:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 
-let drivers = ['kirmanj', 'fars', 'web', 'recoded']
+let drivers = ['kirmanj', 'fars', 'web', 'recoded'];
 
 
 const returnFirstTwoDrivers = Function (drivers){
